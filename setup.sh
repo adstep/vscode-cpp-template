7a9d9a1b6e13072ab109329356ec13cf2044d8f0
Adam Stephenson <adstep@microsoft.com>
2019-10-18 12:04:29 -0700

    fixing setup.sh to correctly use project name

--- setup.sh
+++ setup.sh
@@ -3,7 +3,5 @@
 echo -n "Project name: "
 read project
 
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sed -i -E 's@("program": ")(.*)(",)@\1'"$scriptDir"'\/build\/'"$project"'\3@' .vscode/launch.json
+sed -i -E 's@("program": ")(.*)(",)@\1'"$project"'\/build\/'"$project"'\3@' .vscode/launch.json
 sed -i -E 's@(TARGET_EXEC \?= )(.*)@\1'"$project"'@' Makefile
